f4725994e0ef896a7d7caba7e0837bda010edeea
Srinivas Chowdhury <chowdhu@us.ibm.com>
2017-06-12 13:10:49 -0500

    updated to no prompt

--- SL/installStress.sh
+++ SL/installStress.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo wget http://dl.fedoraproject.org/pub/epel/6/x86_64/stress-1.0.4-4.el6.x86_64.rpm
-sudo yum install stress-1.0.4-4.el6.x86_64.rpm 
+sudo yum -y install stress-1.0.4-4.el6.x86_64.rpm 
 stress -c 1 -m 1 --vm-bytes 300M > /dev/null 2>&1 &
