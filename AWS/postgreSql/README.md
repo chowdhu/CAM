6d737adcc6ba64b3607c226c1a9e0a7843b719f4
Srinivas Chowdhury <chowdhu@us.ibm.com>
2017-12-17 22:42:11 -0600

    Done

--- AWS/postgreSql/README.md
+++ AWS/postgreSql/README.md
@@ -3,14 +3,14 @@
 
 **Required Inputs**
 * AWS Access Key and Access Secret
-* AWS subnet id where the worker node will be provisioned
-* AWS private key to talk to the provisioned worker node
-* AMI of the image
-* Security group with 8080 open
+* Verison of PostgreSQL
+* Db name of PostgreSQL
+* userid and password of PostgreSQL
+* Default port 5432
 
 **Import to CAM**
 * Create a cloud connection with AWS Access key and Secret
-* Menu ==> Templates ==> Create Template ==> From GitHub => Enter the following ino
+* Menu ==> Templates ==> Create Template ==> From GitHub => Enter the following info
   * GIT URL: https://github.com/chowdhu/CAM
   * GitHub Access Token: <>
   * GitHub Repository sub-directory: /AWS/postgreSql
--- AWS/postgreSql/main.tf
+++ AWS/postgreSql/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 variable "aws_dbversion" {
     description = "PostgreSQl engine version"
-    default     = "9.4.4"
+    default     = "9.6.5"
 }
 variable "aws_instancetype" {
     description = "AWS Instance type"
@@ -23,16 +23,18 @@ variable "aws_dbpwd" {
 }
 resource "aws_db_instance" "postgresql" {
 
-  allocated_storage    = "10"
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "${var.aws_dbversion}"
-  instance_class       = "${var.aws_instancetype}"
-  name                 = "${var.aws_dbname}"
-  username             = "${var.aws_dbuid}"
-  password             = "${var.aws_dbpwd}"
-  multi_az             = false
-  storage_encrypted    = false
+  allocated_storage         = "10"
+  storage_type              = "gp2"
+  engine                    = "postgres"
+  engine_version            = "${var.aws_dbversion}"
+  instance_class            = "${var.aws_instancetype}"
+  name                      = "${var.aws_dbname}"
+  username                  = "${var.aws_dbuid}"
+  password                  = "${var.aws_dbpwd}"
+  multi_az                  = false
+  storage_encrypted         = false
+  skip_final_snapshot       = true
+  final_snapshot_identifier = "terraform-aws-postgresql-rds-snapshot"
 
   tags {
     Name = "ChowPostgreSQL"
