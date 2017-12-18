4212d8d1b0440cc535a09cac0fe393b0321c8f10
Srinivas Chowdhury <chowdhu@us.ibm.com>
2017-12-17 22:59:49 -0600

    output

--- AWS/postgreSql/main.tf
+++ AWS/postgreSql/main.tf
@@ -42,6 +42,16 @@ resource "aws_db_instance" "postgresql" {
   }
 }
 
-output "New node ip address " {
-  value = "${aws_db_instance.postgresql.public_ip}"
+
+output "id" {
+  value = "${aws_db_instance.postgresql.id}"
+}
+output "hostname" {
+  value = "${aws_db_instance.postgresql.address}"
+}
+output "port" {
+  value = "${aws_db_instance.postgresql.port}"
+}
+output "endpoint" {
+  value = "${aws_db_instance.postgresql.endpoint}"
 }
